cef8e0fb96b3d306036893aedbee4843190bf6dc
qerogram <qerogram@naver.com>
2021-01-19 19:15:14 +0900

    Implementation : Classification data

--- app.py
+++ app.py
@@ -2,18 +2,32 @@ import tensorflow as tf
 import pandas as pd
 
 class DeepLearning :
-  def __init__(self, path, n) :
+  REGRESSION_MODE = 1
+  CLASSIFICATION_MODE = 2
+  # mode = 1 : regression
+  # mode = 2 : classification
+  def __init__(self, path, n, mode) :
+    self.mode = mode
     self.data = pd.read_csv(path)
-    self.독립 = self.data[ [_ for _ in self.data.columns[:n] ] ]    
+
+    # one hot encoding
+    if mode == self.CLASSIFICATION_MODE : self.data = pd.get_dummies(self.data)
+
+    self.독립 = self.data[ [_ for _ in self.data.columns[:n] ] ]
     self.종속 = self.data[ [_ for _ in self.data.columns[n:] ] ]
     
   
   def compileModel(self) :
     X = tf.keras.layers.Input(shape=[len(self.독립.columns)])
-    Y = tf.keras.layers.Dense(len(self.종속.columns))(X)
+
+    if self.mode == self.REGRESSION_MODE :
+      Y = tf.keras.layers.Dense(len(self.종속.columns))(X)
+    else :
+      Y = tf.keras.layers.Dense(len(self.종속.columns), activation='softmax')(X)
 
     self.model = tf.keras.models.Model(X, Y)
-    self.model.compile(loss='mse')
+    self.model.compile(loss='mse' if self.mode == self.REGRESSION_MODE else 'categorical_crossentropy',
+                       metrics='accuracy')
     
   
   def learningData(self, epochs=10, verbose=True) :
@@ -24,8 +38,8 @@ class DeepLearning :
 
 
 if __name__ == "__main__" :
-	d = DeepLearning("https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/boston.csv", 13)
+  d = DeepLearning('https://raw.githubusercontent.com/blackdew/tensorflow1/master/csv/iris.csv', 4, DeepLearning.CLASSIFICATION_MODE)
 	d.compileModel()
 	d.learningData(epochs=1000, verbose=False)
-  d.predictData(d.독립[5:10])
-  print(d.종속[5:10])
+  d.predictData(d.독립[-5:])
+  print(d.종속[-5:])
